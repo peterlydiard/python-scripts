bfdf97c50f46b1843464f86ae969fc6b3ac4eb0f
peterlydiard <peter_lydiard@yahoo.co.uk>
2023-09-15 23:02:13 +0100

    Only backup changed files.

--- backup.py
+++ backup.py
@@ -21,6 +21,20 @@ def calculate_hash(file_path):
             hasher.update(data)
     return hasher.hexdigest()
 
+# Function to build a list of MD5 hashes from existing backup database files
+def build_existing_hash_list(backup_base_dir):
+    existing_hashes = set()  # Use a set for efficient lookup
+    for root, _, files in os.walk(backup_base_dir):
+        for file in files:
+            if file.startswith("backup_database_") and file.endswith(".txt"):
+                database_file = os.path.join(root, file)
+                with open(database_file, 'r') as db:
+                    lines = db.readlines()
+                    for i in range(2, len(lines), 4):
+                        md5_hash = lines[i].strip().split(": ")[1]
+                        existing_hashes.add(md5_hash)
+    return existing_hashes
+
 # Function to perform an incremental backup
 def incremental_backup(source_dir, backup_base_dir):
     # Get the current date as a string (e.g., "2023-09-15")
@@ -37,22 +51,26 @@ def incremental_backup(source_dir, backup_base_dir):
     # Create a list to store information about backed up files
     backup_info = []
 
+    # Build a set of existing MD5 hashes
+    existing_hashes = build_existing_hash_list(backup_base_dir)
+
     for root, _, files in file_system:
         for file in files:
             source_file = os.path.join(root, file)
             relative_path = os.path.relpath(source_file, source_dir)
             backup_file = os.path.join(backup_dir, relative_path)
+            
+            # Calculate the MD5 hash of the source file
+            file_hash = calculate_hash(source_file)
 
-            if not os.path.exists(backup_file) or calculate_hash(source_file) != calculate_hash(backup_file):
+            if file_hash not in existing_hashes:
                 os.makedirs(os.path.dirname(backup_file), exist_ok=True)
                 shutil.copy2(source_file, backup_file)
                 print(f"Backed up: {relative_path} to {backup_dir}")
 
-                # Calculate the MD5 hash of the source file
-                file_hash = calculate_hash(source_file)
-
                 # Add file information to the backup_info list
                 backup_info.append((source_file, backup_file, file_hash))
+
     
     # Create and save the backup database text file
     database_file = os.path.join(backup_base_dir, f"backup_database_{current_date}.txt")

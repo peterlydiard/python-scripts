1c4d6f84f83274c33d9dce3a083af94e0ce5c422
peterlydiard <peter_lydiard@yahoo.co.uk>
2023-09-18 22:39:08 +0100

    Remove unnecessary prompt quotes.

--- backup.bat
+++ backup.bat
@@ -1,6 +1,6 @@
 @echo off
 CLS
-ECHO "Backup running. Please wait."
+ECHO Backup running. Please wait.
 C:\Users\Pete\AppData\Local\Programs\Python\Python311\python.exe "C:\Users\Pete\Tools\backup.py"
 ECHO Backup complete.
 timeout 5 > nul

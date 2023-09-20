50bbca9373c9d6b7c4fc9355226b58f0fd736b85
peterlydiard <peter_lydiard@yahoo.co.uk>
2023-09-20 17:02:11 +0100

    Delete Windows files from main branch.

--- backup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-CLS
-ECHO Backup running. Please wait.
-C:\Users\Pete\AppData\Local\Programs\Python\Python311\python.exe "C:\Users\Pete\Tools\backup.py"
-ECHO Backup complete.
-timeout 5 > nul
-exit
--- backup_config_windows.py
+++
@@ -1,8 +0,0 @@
-# List of source directories to be backed up
-source_dirs = ['C:\\Users\\Pete\\OneDrive\\Documents']
-
-# List of directories to be excluded from backup
-excluded_dirs = []
-
-# Backup base directory
-backup_base_dir = 'D:\\Users\\Pete\\File History'

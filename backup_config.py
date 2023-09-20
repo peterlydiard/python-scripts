5e604cb3de43b5ec701ad2d1cd084970a45b0ca2
Peter <peter_lydiard@yahoo.co.uk>
2023-09-20 11:56:02 +0100

    Changes for Linux live flash drive.

--- backup_config.py
+++ backup_config.py
@@ -1,6 +1,6 @@
 # List of source directories to be backed up
 source_dirs = ['/home/peter/Pictures', '/home/peter/bash']
-backup_base_dir = '/home/peter/History'
+backup_base_dir = '/home/peter/File History'
 
 # List of directories to be excluded from backup
 excluded_dirs = ['/home/peter/Pictures/Saved Pictures']
